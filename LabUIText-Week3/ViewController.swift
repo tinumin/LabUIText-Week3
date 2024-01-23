08b0f2a43485eb4a3b3cc016b85d01227b5ff658
Tinu <tinukudassanadu@gmail.com>
2024-01-23 17:10:45 -0500

    Initial Commit

---
+++ LabUIText-Week3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabUIText-Week3
+//
+//  Created by user237087 on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
